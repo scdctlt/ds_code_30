551bd98f4e7cc3c3d0df9ad7d86e6a1f079ddbcf
scdctlt <scdctlt@163.com>
2022-11-23 10:22:13 +0800

    任务更新

---
+++ PPOJ/树/1374_层数最深叶子结点的和.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef struct BiNode
+{
+    int data;
+    
+}BiNode,*BiTree;
+// 1.计算最大层次数目
+// 2.遍历每个结点判断是否是最大层数
+// 3.是结点最大层数的
+
+void build_Tree(BiTree &t,int array[]){
+    
+}
+
+int sum_lef;
+void count_lef(BiTree &t){
+
+}
